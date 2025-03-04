035971cea733ba347596160692f9c0153ba8fa3f
Амелькин Владимир <amelkin@vbankcenter.ru>
2025-03-04 10:54:38 +0300

    Initial commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Владимир Амелькин on 04.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
